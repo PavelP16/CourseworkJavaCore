f3e6fb26babacc3e1ff3d0cfc413e2890c415cc5
Pavel <pavelpulle1@gmail.co>
2024-09-22 21:50:42 +0300

    Initial commit

---
+++ CourseworkJavaCore/src/main/java/com/example/CourseworkJavaCore/CourseworkJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseworkJavaCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkJavaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkJavaCoreApplication.class, args);
+	}
+
+}
---
+++ CourseworkJavaCore/src/test/java/com/example/CourseworkJavaCore/CourseworkJavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseworkJavaCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkJavaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
